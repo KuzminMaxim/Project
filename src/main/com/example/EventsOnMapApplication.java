5d6ab692606bee405cea48ae880ab64e4160bc30
KuzminMaxim <kuzminqwe75@icloud.com>
2019-12-11 22:20:14 +0300

    Legacy code fragments removed.

--- src/main/com/example/EventsOnMapApplication.java
+++ src/main/com/example/EventsOnMapApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JDBCApplication {
+public class EventsOnMapApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JDBCApplication.class, args);
+        SpringApplication.run(EventsOnMapApplication.class, args);
     }
 }
